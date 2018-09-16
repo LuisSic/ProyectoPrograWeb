0bfedf6b50eaeeb3d99a5f66d1b560595db3656c
LuisSic <lantoni24@hotmail.com>
2018-09-15 22:18:54 -0600

    arreglo de ver catalogo

--- angular-web-luis/src/app/catalogo/catalogo.component.ts
+++ angular-web-luis/src/app/catalogo/catalogo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {VIDEOGAMES} from '../mock-videogame';
 
 @Component({
   selector: 'app-catalogo',
@@ -6,10 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./catalogo.component.css']
 })
 export class CatalogoComponent implements OnInit {
+  videogames = VIDEOGAMES;
 
   constructor() { }
 
   ngOnInit() {
   }
-
 }
---
+++ angular-web-luis/src/app/mock-videogame.ts
@@ -0,0 +1,10 @@
+import { Videogame } from './videogame';
+
+export const VIDEOGAMES: Videogame[] = [
+  { id: 1, name: 'Battlefield', description: 'Esto es una prueba para ver como funciona Battlefield'},
+  { id: 2, name: 'Halo 5', description: 'Esto es una prueba para ver como funciona Halo 5'},
+  { id: 3, name: 'Halo Wars', description: 'Esto es una prueba para ver como funciona Halo wars'},
+  { id: 4, name: 'Fifa 18', description: 'Esto es una prueba para ver como funciona Fifa 18'},
+  { id: 5, name: 'Call of duty', description: 'Esto es una prueba para ver como funciona call of rata'},
+  { id: 6, name: 'Star Wars Battlefront', description: 'Esto es una prueba para ver como funciona satar wars'}
+];
